eb1d0f4a832dfe7885a8b000e7845f22def46c80
Boian Mihailov <boian.mihailov@gmail.com>
2018-06-26 16:02:17 +0300

    add channel

--- vars/clientSlackNotifier.groovy
+++ vars/clientSlackNotifier.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 
-def call(String buildResult, tokenID, baseUrl) {
+def call(String buildResult, String tokenID, String baseUrl, String channel) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful", baseUrl: baseUrl, tokenCredentialId: tokenID
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful", baseUrl: baseUrl, tokenCredentialId: tokenID, channel: channel
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed", baseUrl: baseUrl, tokenCredentialId: tokenID
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed", baseUrl: baseUrl, tokenCredentialId: tokenID, channel: channel
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable", baseUrl: baseUrl, tokenCredentialId: tokenID
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable", baseUrl: baseUrl, tokenCredentialId: tokenID, channel: channel
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear", baseUrl: baseUrl, tokenCredentialId: tokenID
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear", baseUrl: baseUrl, tokenCredentialId: tokenID, channel: channel
   }
 }
